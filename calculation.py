20d3c0ef00a50fff35c6fec7d18ed5d2c8345d21
Dmitry <40293610+Djerys@users.noreply.github.com>
2019-06-22 01:18:24 +0300

    Added fcnf and fdnf properties in BooleanCalculator

--- calculation.py
+++ calculation.py
@@ -1,6 +1,14 @@
+from functools import reduce
+
+import boolean_ast as ast
+
+
 class BooleanCalculator(object):
     def __init__(self, ast):
         self._ast = ast
+        self._truth_table = None
+        self._fcnf = None
+        self._fdnf = None
 
     @property
     def ast(self):
@@ -8,17 +16,64 @@ class BooleanCalculator(object):
 
     @property
     def truth_table(self):
-        variables = self.ast.variables
-        variables = list(variables)
-        variables.sort()
-        variable_values_list = [dict(zip(variables, subset))
-                                for subset in _subsets(len(variables))]
+        if self._truth_table is None:
+            variables = list(self.ast.variables)
+            variables.sort()
+            variable_values_list = [dict(zip(variables, subset))
+                                    for subset in _subsets(len(variables))]
 
-        table = []
-        for variable_values in variable_values_list:
-            variable_values['F'] = self.ast.calculate(variable_values)
-            table.append(variable_values)
-        return table
+            self._truth_table = []
+            for variable_values in variable_values_list:
+                variable_values['F'] = self.ast.calculate(variable_values)
+                self._truth_table.append(variable_values)
+
+        return self._truth_table
+
+    @property
+    def fcnf(self):
+        if self._fcnf is None:
+            truth_table = self.truth_table
+            nodes = [self._fcnf_sum(row) for row in truth_table
+                     if self._fcnf_sum(row) is not None]
+            self._fcnf = reduce(lambda a, x: a & x, nodes)
+
+        return self._fcnf
+
+    @property
+    def fdnf(self):
+        if self._fdnf is None:
+            truth_table = self.truth_table
+            nodes = [self._fdnf_product(row) for row in truth_table
+                     if self._fdnf_product(row) is not None]
+            self._fdnf = reduce(lambda a, x: a | x, nodes)
+
+        return self._fdnf
+
+    def _fcnf_sum(self, variable_values):
+        if variable_values['F']:
+            return None
+        nodes = []
+        for variable in set(variable_values) - {'F'}:
+            if variable_values[variable]:
+                nodes.append(
+                    ast.NotExpression(ast.VariableExpression(variable)))
+            else:
+                nodes.append(ast.VariableExpression(variable))
+        fcnf_sum = reduce(lambda a, x: a | x, nodes)
+        return fcnf_sum
+
+    def _fdnf_product(self, variable_values):
+        if not variable_values['F']:
+            return None
+        nodes = []
+        for variable in set(variable_values) - {'F'}:
+            if variable_values[variable]:
+                nodes.append(ast.VariableExpression(variable))
+            else:
+                nodes.append(
+                    ast.NotExpression(ast.VariableExpression(variable)))
+        fdnf_product = reduce(lambda a, x: a & x, nodes)
+        return fdnf_product
 
 
 def _subsets(power):
@@ -26,7 +81,3 @@ def _subsets(power):
         str_subset = bin(i).lstrip('0b').rjust(power, '0')
         subset = [int(x) for x in str_subset]
         yield subset
-
-
-if __name__ == '__main__':
-    print(list(_subsets(3)))
